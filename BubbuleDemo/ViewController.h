dffb80b37407fcab5009e9e7f4cf69f29652f548
shenkuikui <kuikui.shen@gmail.com>
2015-11-28 14:33:37 +0800

    Init
    begin  bubble demo

---
+++ BubbuleDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BubbuleDemo
+//
+//  Created by shenkuikui on 15/11/28.
+//  Copyright © 2015年 Ewing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BubbuleDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BubbuleDemo
+//
+//  Created by shenkuikui on 15/11/28.
+//  Copyright © 2015年 Ewing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
