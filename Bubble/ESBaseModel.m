045e2b5d38d425b8086c55b283366d4e1e8d6ec9
shenkuikui <kuikui.shen@gmail.com>
2015-11-30 08:17:25 +0800

    add base model

---
+++ Bubble/ESBaseModel.m
@@ -0,0 +1,34 @@
+//
+//  ESBaseModel.m
+//  BubbuleDemo
+//
+//  Created by shenkuikui on 15/11/28.
+//  Copyright © 2015年 Ewing. All rights reserved.
+//
+
+#import "ESBaseModel.h"
+
+@implementation ESBaseModel
+
++ (NSString *)createTableSQL
+{
+    return nil;
+}
+
++ (NSString *)tableName
+{
+    return nil;
+}
+
++ (id)primaryKey
+{
+    return nil;
+}
+
++ (NSString *)modelSign
+{
+    return nil;
+}
+
+
+@end
